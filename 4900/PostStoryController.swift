25128d44c8637002f94ffa255600744c2c27c056
Laura Yang <laurayang@lauras-imac.hitronhub.home>
2017-05-07 00:31:56 -0700

    Connect the PostStoryController with the bottom tab bar, fixed the tabBar crash issue

---
+++ 4900/PostStoryController.swift
@@ -0,0 +1,16 @@
+//
+//  PostStoryController.swift
+//  4900
+//
+//  Created by Laura Yang on 2017-05-06.
+//  Copyright © 2017 leo  luo. All rights reserved.
+//
+
+import UIKit
+
+class PostStoryController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
